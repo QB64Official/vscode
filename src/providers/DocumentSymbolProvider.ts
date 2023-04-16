230393b7f089c05e712c639cdcfce068a9b116d0
grymmjack <grymmjack@gmail.com>
2023-04-16 17:46:42 -0400

    Improved support for DocumentSymbolProvider - see this video: https://app.screencast.com/UAZ7zYPxM9RC6
    Improvements:
    Old was skipping leading whitespace subs, funcs, etc. New fixes that.
    
    Additions:
    Added support for declare versions of sub and function
    Added support for constants (including showing their value)
    Added support for $include's
    Added support for types
    Added support for labels and loop constructs

--- src/providers/DocumentSymbolProvider.ts
+++ src/providers/DocumentSymbolProvider.ts
@@ -13,17 +13,55 @@ export class DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
 			for (let i = 0; i < document.lineCount; i++) {
 				const line = document.lineAt(i);
-
-				if (line.text.toLowerCase().startsWith("sub ") || line.text.toLowerCase().startsWith("function ")) {
-
-					let isSub: boolean = line.text.toLowerCase().startsWith("sub ");
-					let tokens: string[] = line.text.split(" ");
+				let tokens: string[] = line.text.trim().split(' ');
+				let lineText = line.text.trim().toLowerCase();
+				let symbol = tokens[1] || "undef";
+				let symbolKind: any = false;
+				if (lineText.startsWith('sub ') || lineText.startsWith('declare sub ')) {
+					symbolKind = vscode.SymbolKind.Function;
+					var symbolText = 'Sub';
+				}
+				if (lineText.startsWith('function ') || lineText.startsWith('declare function ')) {
+					symbolKind = vscode.SymbolKind.Function;
+					var symbolText = 'Function';
+				}
+				if (lineText.startsWith('const ')) {
+					symbolKind = vscode.SymbolKind.Constant;
+					var symbolText = 'Const';
+					if (lineText.indexOf('=')) {
+						var assignment = line.text.trim().split('=');
+						var symbolText = '= ' + assignment[1];
+					}
+				}
+				if (lineText.startsWith("'$include:")) {
+					symbolKind = vscode.SymbolKind.Interface;
+					var pieces = line.text.trim().split(':');
+					symbol = pieces[1];
+					var symbolText = 'Include';
+				}
+				if (lineText.startsWith("type ")) {
+					if (symbol.toLowerCase() != 'as') {
+						symbolKind = vscode.SymbolKind.Class;
+						var symbolText = 'Type';
+					}
+				}
+				if (lineText.endsWith(":")) {
+					symbolKind = vscode.SymbolKind.Event;
+					symbol = line.text.trim();
+					if (symbol.toLowerCase() != 'do:' && symbol.toLowerCase() != 'while:') {
+						var symbolText = 'Label';
+					} else {
+						var symbolText = 'Loop';
+					}
+				}
+				if (symbolKind != false) {
 					let marker_symbol = new vscode.DocumentSymbol(
-						tokens[1].trim(),
-						isSub ? "Sub" : "Function",
-						isSub ? vscode.SymbolKind.Method : vscode.SymbolKind.Function,
-						line.range, line.range);
-
+						symbol,
+						symbolText,
+						symbolKind,
+						line.range, 
+						line.range
+					);
 					nodes[nodes.length - 1].push(marker_symbol);
 				}
 			}
